627c1a19034faf23a16beb2351bbe91f9e8afbdd
Namrata Jaiswal <76630286+NAMRATA1214@users.noreply.github.com>
2022-07-07 13:02:55 +0530

    Add files via upload
    Identify the data types and name the variables

---
+++ IdentifyDataTypes.java
@@ -0,0 +1,41 @@
+/* 1.Paul has bought a new notebook of "idol" brand, with a processor speed of 2.66GHz, 8 GB of RAM, 500GB hard disk, with a 15-inch monitor, for $1760.55. 
+He has chosen service plan 'C' among plans 'A', 'B', 'C', and 'D', plus on-site servicing but did not choose extended warranty. 
+Identify the data types and name the variables.*/
+
+
+//Ans==> 
+
+class IdentifyDataTypes
+	
+{
+		public static void main(String[] args)
+			{
+
+				// Identify the data types and name the variables.
+
+	
+				String name = "Paul";
+
+				String brand = "idol";
+
+				double processorspeedInGHz = 2.66;
+
+				float ramSizeInGB = 8;
+
+				int HardDiskInGB = 500;
+
+				byte InchMonitor = 15;
+
+				double rupeesIn$ = 1760.55;
+
+				char servicePlan = 'C';
+
+				boolean OnSiteServicing = true;
+
+				boolean ExtendedWarranty = false;
+
+			}
+}	
+	
+
+
