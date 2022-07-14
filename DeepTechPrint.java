64bde0b33d3c4d1dd807571c3a3c95ba7a1de54c
Namrata Jaiswal <76630286+NAMRATA1214@users.noreply.github.com>
2022-07-14 13:11:20 +0530

    Add files via upload
    DeepTechPrint Program

---
+++ DeepTechPrint.java
@@ -0,0 +1,36 @@
+/*	4.Prints the numbers from 1 to 100 such that:
+
+	If the number is a multiple of 3, you need to print "Deep" instead of that number.
+	If the number is a multiple of 5, you need to print "Tech" instead of that number.
+	If the number is a multiple of both 3 and 5, you need to print "DeepTech" instead of that number.	*/
+
+//--->
+class DeepTechPrint
+	{
+		public static void main(String[] args)
+			{
+				System.out.println("Enter the Number from 1 to 100 is :");
+				for(int i=1;i<=100;i++)
+					{
+
+						if(i%3==0 && i%5==0)
+							{
+								System.out.println("DeepTech");
+							}
+					
+						else if(i%3==0)
+							{
+								System.out.println("Deep");
+							}
+						else if(i%5==0)
+							{
+								System.out.println("Tech");		
+							}
+						else
+						{
+						System.out.println(+i);
+						}
+						
+					}
+			}
+	}
